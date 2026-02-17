9a68fa2e13326a30e8e3e40c43c51965ba91d475
Mohamed Nasser <423232537@sha.edu.eg>
2026-02-18 00:31:36 +0200

    Fix types for UP.tsx and Dispatch/SetStateAction imports

--- src/Subject to/User Information/CreateAcont/Up/UP.tsx
+++ src/Subject to/User Information/CreateAcont/Up/UP.tsx
@@ -1,22 +1,21 @@
 import { SignUpPage } from "../Customer Account/Customer_Account";
 import { CompanyRegister } from "../Company_Account/Company_Account";
-import { useState } from "react";
-
-export default function UI({
-  setIsLogin,
-  isCustomer,
-  setIsCustomer,
-}: {
+import type { Dispatch, SetStateAction } from "react";
+interface UIProps {
   isCustomer: boolean;
-  setIsLogin: (isLogin: boolean) => void;
-  setIsCustomer: (isCustomer: boolean) => void;
-}) {
+  setIsLogin: Dispatch<SetStateAction<boolean>>;
+  setIsCustomer: Dispatch<SetStateAction<boolean>>;
+}
 
-
+export default function UI({ setIsLogin, isCustomer, setIsCustomer }: UIProps) {
   return (
     <>
       {isCustomer ? (
-        <SignUpPage isCustomer={isCustomer} setIsCustomer={setIsCustomer} setIsLogin={setIsLogin} />
+        <SignUpPage
+          isCustomer={isCustomer}
+          setIsCustomer={setIsCustomer}
+          setIsLogin={setIsLogin}
+        />
       ) : (
         <CompanyRegister
           setIsLogin={setIsLogin}
